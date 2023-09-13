5996f860006101423b474e52567999bbcb649f0e
Bjørnar <598062@stud.hvl.no>
2023-09-14 00:10:55 +0200

    nokken filer

---
+++ Oppg1/src/no/hvl/dat108/HelloThread.java
@@ -0,0 +1,9 @@
+package no.hvl.dat108;
+
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class HelloThread {
+	public static void main(String[] args) {
+		String input = showInputDialog("Skriv inn melding, eller quit for å avslutte");
+	}
+}
---
+++ Oppg2/src/no/hvl/dat108/Hamburgersjappe.java
@@ -0,0 +1,4 @@
+package no.hvl.dat108;
+
+public class Hamburgersjappe {
+}
